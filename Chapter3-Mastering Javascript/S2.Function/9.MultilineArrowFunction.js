f9cb2448c7712940ad4d5f0153980f2f9f2d8785
gungunfebrianza <pusatrisetteknologi@gmail.com>
2019-06-28 19:37:22 +0700

    Multiline Arrow Function

---
+++ Chapter3-Mastering Javascript/S2.Function/9.MultilineArrowFunction.js
@@ -0,0 +1,8 @@
+
+let sum = (a, b) => { // curly brace untuk membuat multiline function
+  let result = a + b;
+  return result; // gunakan return untuk memproduksi hasil
+};
+
+console.log(sum(1, 2)); // 3
+
