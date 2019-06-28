a8c652f3582790c14e6d058c3bda19601313e3b7
gungunfebrianza <pusatrisetteknologi@gmail.com>
2019-06-28 12:27:56 +0700

    Add Another Example

--- Chapter3-Mastering Javascript/16/break-directive.js
+++ Chapter3-Mastering Javascript/16/break-directive.js
@@ -1,9 +1,24 @@
 for (i = 0; i < 10; i++) {
   if (i === 3) { break; }
   console.log("Perulangan ke " + i);
-} 
+}
 /* Output :
 Perulangan ke 0
 Perulangan ke 1
 Perulangan ke 2
 */
+
+/*
+//Example #1 :
+//Remove the comment [SHIFT+ALT+A]:
+var i = 0;
+
+while (i < 6) {
+  if (i === 3) {
+    break;
+  }
+  i = i + 1;
+}
+
+console.log(i);
+//output: 3 */
