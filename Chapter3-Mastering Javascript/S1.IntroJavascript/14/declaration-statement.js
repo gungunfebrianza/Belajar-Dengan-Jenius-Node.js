7d7f6e50ec3c0e6713998b1854e8b0daea0408e8
gungunfebrianza <pusatrisetteknologi@gmail.com>
2019-06-28 15:22:51 +0700

    Create Simple Function

--- Chapter3-Mastering Javascript/S1.IntroJavascript/14/declaration-statement.js
+++ Chapter3-Mastering Javascript/S1.IntroJavascript/14/declaration-statement.js
@@ -1,2 +1,2 @@
-let x; //non-expression statement
+let x; //declaration statement
 x = 2 * 10; //expression statement
---
+++ Chapter3-Mastering Javascript/S2.Function/1.Function.js
@@ -0,0 +1,4 @@
+function sayHello() {
+  console.log('Hello!');
+  console.log('Gun Gun Febrianza!');
+}
