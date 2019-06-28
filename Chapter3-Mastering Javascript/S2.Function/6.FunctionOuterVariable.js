b8ef3f6bcde671f719f7cb229a4d53d4437b88f7
gungunfebrianza <pusatrisetteknologi@gmail.com>
2019-06-28 17:00:34 +0700

    New Example

---
+++ Chapter3-Mastering Javascript/S2.Function/6.FunctionOuterVariable.js
@@ -0,0 +1,8 @@
+let userName = 'Gun Gun Febrianza';
+
+function tampilkanPesan() {
+  let message = 'Hello, ' + userName;
+  console.log(message);
+}
+
+tampilkanPesan(); // Hello, Gun Gun Febrianza
