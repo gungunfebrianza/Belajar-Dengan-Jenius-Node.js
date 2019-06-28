8c6133e8023fd11f642d50159058f0f79a7ffbfb
gungunfebrianza <pusatrisetteknologi@gmail.com>
2019-06-28 12:59:24 +0700

    New Example

--- Chapter3-Mastering Javascript/16/label-statement.js
+++ Chapter3-Mastering Javascript/16/label-statement.js
@@ -10,3 +10,30 @@ for (var i = 0; i < 5; i++) {
 
 console.log(str);
 //output: "0234"
+
+
+/*
+//Example #1 :
+//Remove the comment [SHIFT+ALT+A]:
+var i, j;
+
+loop1:
+for (i = 0; i < 3; i++) {      //The first for statement is labeled "loop1"
+  loop2:
+  for (j = 0; j < 3; j++) {   //The second for statement is labeled "loop2"
+    if (i === 1 && j === 1) {
+      continue loop1;
+    }
+    console.log('i = ' + i + ', j = ' + j);
+  }
+}
+
+// Output is:
+//   "i = 0, j = 0"
+//   "i = 0, j = 1"
+//   "i = 0, j = 2"
+//   "i = 1, j = 0"
+//   "i = 2, j = 0"
+//   "i = 2, j = 1"
+//   "i = 2, j = 2"
+// Notice how it skips both "i = 1, j = 1" and "i = 1, j = 2" */
