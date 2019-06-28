570b241ba622b9402cb15358223d50b1f6fc07e5
gungunfebrianza <pusatrisetteknologi@gmail.com>
2019-06-28 11:00:19 +0700

    New Script

---
+++ Chapter3-Mastering Javascript/16/for.js
@@ -0,0 +1,10 @@
+for (let index = 0; index < 5; index++) {
+  console.log("Perulangan ke " + index);
+}
+/* Output :
+Perulangan ke 0
+Perulangan ke 1
+Perulangan ke 2
+Perulangan ke 3
+Perulangan ke 4 
+*/
