e3ab54104da79f1e451d3d4c30d803f8f6610ed2
gungunfebrianza <pusatrisetteknologi@gmail.com>
2019-06-28 15:54:47 +0700

    Add FUnction Return

---
+++ Chapter3-Mastering Javascript/S2.Function/2.FunctionParameter.js
@@ -0,0 +1,11 @@
+function tampilkanPesan(from, text) {
+  // arguments: from, text
+  console.log(from + ': ' + text);
+}
+
+tampilkanPesan('Kaiz', 'Hello! Racheysa!'); 
+tampilkanPesan('Racheysa', "What's up? kaiz!"); 
+/* 
+Output
+Kaiz: Hello! Racheysa!
+Racheysa: What's up? kaiz! */
