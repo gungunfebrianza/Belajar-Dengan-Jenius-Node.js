81604b3a61a4b8112f199cbdb09bd10d36596ca0
gungunfebrianza <pusatrisetteknologi@gmail.com>
2019-07-02 20:36:49 +0700

    Add Comment

---
+++ Chapter3-Mastering Javascript/S1.IntroJavascript/1/comment.js
@@ -0,0 +1,11 @@
+// Single Line Comment
+// Short cut in atom & visual studio code is CTRL + /
+
+/*
+      Multi
+      Line
+      Comment
+      */
+
+/* To make multi comment shortcut
+in atom & visual studio code is SHIFT + ALT + A */
