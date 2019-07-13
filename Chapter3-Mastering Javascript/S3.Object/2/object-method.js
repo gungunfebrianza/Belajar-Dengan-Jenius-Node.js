60fd012ec30c9f1288ee6104bb3d19f74d029618
gungunfebrianza <pusatrisetteknologi@gmail.com>
2019-07-14 01:19:46 +0700

    Add Example

---
+++ Chapter3-Mastering Javascript/S3.Object/2/object-method.js
@@ -0,0 +1,6 @@
+var cantik = { firstname: 'Maudy', lastname: 'Ayunda Faza', age: 23, haircolor: 'black', fullname: function () { console.log("Maudy Ayunda Faza"); } }
+cantik.fullname();
+/*
+Output :
+Maudy Ayunda Faza
+ */
