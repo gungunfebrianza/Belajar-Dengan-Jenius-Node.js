60f83fc8f8c3c6eeee10e7f5bf5ae6cdf66aae36
gungunfebrianza <pusatrisetteknologi@gmail.com>
2019-07-14 04:52:39 +0700

    Add Solution

--- Chapter3-Mastering Javascript/S3.Object/2/5.object-prototype.js
+++ Chapter3-Mastering Javascript/S3.Object/2/5.object-prototype.js
@@ -1,4 +1,5 @@
 
+/* Problem
 function Person(firstname, lastname, age, eyecolor) {
   this.firstname = firstname;
   this.lastname = lastname;
@@ -10,4 +11,18 @@ Person.skill = "singing";
 
 var maudy = new Person("Maudy", "Ayunda Faza", 23, "Black")
 
-console.log(maudy.skill); //undefined
+console.log(maudy.skill); //undefined 
+*/
+
+function Person(firstname, lastname, age, eyecolor) {
+  this.firstname = firstname;
+  this.lastname = lastname;
+  this.age = age;
+  this.eyecolor = eyecolor;
+}
+
+Person.prototype.skill = "singing";
+
+var maudy = new Person("Maudy", "Ayunda Faza", 23, "Black")
+
+console.log(maudy.skill); //singin
