d2d038e62f6a214f785ead147e398b0176632ba7
gungunfebrianza <pusatrisetteknologi@gmail.com>
2019-07-14 01:10:55 +0700

    Add Object Initializer

---
+++ Chapter3-Mastering Javascript/S3.Object/2/object-initializer.js
@@ -0,0 +1,11 @@
+var cantik = { firstname: 'Maudy', lastname: 'Ayunda Faza', age: 23, haircolor: 'black' }
+console.log(cantik);
+/*
+Output :
+{
+  firstname: 'Maudy',
+  lastname: 'Ayunda Faza',
+  age: 23,
+  haircolor: 'black'
+}
+ */
