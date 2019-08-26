313d8d2c6ed8ad9e83376d11b33d6b8e1f88af86
gungunfebrianza <pusatrisetteknologi@gmail.com>
2019-08-27 01:57:07 +0700

    Update

---
+++ Chapter3-Mastering Javascript/S1.IntroJavascript/7/primitive-value.js
@@ -0,0 +1,11 @@
+
+    var animal1 = "dinosaurus";
+    var animal2 = animal1;
+
+    console.log(animal1); //dinosaurus
+    console.log(animal2); //dinosaurus
+
+    animal1 = "godzila"
+    console.log(animal1); //godzila
+    console.log(animal2); //dinosaurus
+
