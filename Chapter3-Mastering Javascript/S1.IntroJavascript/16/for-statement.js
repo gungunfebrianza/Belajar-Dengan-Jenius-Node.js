66139e0673ede0572ec4e6db9cfd0cfb84610748
gungunfebrianza <pusatrisetteknologi@gmail.com>
2019-08-27 04:49:41 +0700

    For Statement

---
+++ Chapter3-Mastering Javascript/S1.IntroJavascript/16/for-statement.js
@@ -0,0 +1,22 @@
+    for (let index = 0; index < 5; index++) {
+      console.log("Perulangan ke " + index);
+    }
+    /* Output :
+    Perulangan ke 0
+    Perulangan ke 1
+    Perulangan ke 2
+    Perulangan ke 3
+    Perulangan ke 4
+    */
+
+
+    for (let index = 10; index > 5; index--) {
+      console.log("Perulangan ke " + index);
+    }
+    /* Output :
+    Perulangan ke 10
+    Perulangan ke 9
+    Perulangan ke 8
+    Perulangan ke 7
+    Perulangan ke 6
+    */
