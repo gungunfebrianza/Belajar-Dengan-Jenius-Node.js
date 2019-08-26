ca43ffb6e6a67435766b018262c0ccb7483de0a7
gungunfebrianza <pusatrisetteknologi@gmail.com>
2019-08-27 02:02:16 +0700

    Continue Statement

---
+++ Chapter3-Mastering Javascript/S1.IntroJavascript/16/continue-statement.js
@@ -0,0 +1,12 @@
+
+    for (i = 0; i < 6; i++) {
+      if (i === 3) { continue; }
+      console.log("Perulangan Ke " + i);
+    }
+    // Output
+    // Perulangan Ke 0
+    // Perulangan Ke 1
+    // Perulangan Ke 2
+    // Perulangan Ke 4
+    // Perulangan Ke 5
+
