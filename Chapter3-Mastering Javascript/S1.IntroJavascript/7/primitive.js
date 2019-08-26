17051098dd7c40c57214609d8982ac2b2ef131d5
gungunfebrianza <pusatrisetteknologi@gmail.com>
2019-08-26 14:59:45 +0700

    Update Example

---
+++ Chapter3-Mastering Javascript/S1.IntroJavascript/7/primitive.js
@@ -0,0 +1,16 @@
+// strings
+var name = "Gun Gun Febrianza";
+
+// numbers
+var age = 25;
+var price = 1.51;
+
+// boolean
+var isExist = true;
+
+// null
+var object = null;
+
+// undefined
+var flag = undefined;
+var ref; // setara dengan kode di atas
