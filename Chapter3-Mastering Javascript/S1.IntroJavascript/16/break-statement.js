aab3dac8661f5571bd91cae63d48aa83f39c4ff7
gungunfebrianza <pusatrisetteknologi@gmail.com>
2019-08-27 02:04:08 +0700

    Break Statement

---
+++ Chapter3-Mastering Javascript/S1.IntroJavascript/16/break-statement.js
@@ -0,0 +1,10 @@
+    
+    for (i = 0; i < 10; i++) {
+      if (i === 3) { break; }
+      console.log("Perulangan ke " + i);
+    }
+    /* Output :
+    Perulangan ke 0
+    Perulangan ke 1
+    Perulangan ke 2
+    */
