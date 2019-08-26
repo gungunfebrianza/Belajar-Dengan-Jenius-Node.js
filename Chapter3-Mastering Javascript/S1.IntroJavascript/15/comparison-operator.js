e4ceabe1a2932fc012d4604ed24a7454f50a76b5
gungunfebrianza <pusatrisetteknologi@gmail.com>
2019-08-27 05:02:14 +0700

    Comparison Operator

--- Chapter3-Mastering Javascript/S1.IntroJavascript/15/comparison-operator.js
+++ Chapter3-Mastering Javascript/S1.IntroJavascript/15/comparison-operator.js
@@ -1,4 +1,18 @@
-    //Contoh Return Comparison Operation
-    console.log(4 > 1);  // true (benar)
-    console.log(4 == 2); // false (salah)
-    console.log(4 != 2); // true (benar)
+//Contoh Return Comparison Operation
+console.log(4 > 1);  // true (benar)
+console.log(4 == 2); // false (salah)
+console.log(4 != 2); // true (benar)
+
+
+let result = 11 > 10; // menyimpan hasil perbandingan
+console.log(result); // true
+
+console.log('Z' > 'C'); // true
+console.log('Glow' > 'Glee'); // true
+console.log('Bee' > 'Be'); // true
+
+console.log('20' > 11); // true, string '20' berubah menjadi number 2
+console.log('010' == 10); // true, string '01' berubah menjadi number 1
+
+console.log(true == 1); // true
+console.log(false == 0); // true
