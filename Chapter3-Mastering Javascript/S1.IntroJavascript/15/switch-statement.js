3ca91bd2e3814b5787298e290b767bd9ec205032
gungunfebrianza <pusatrisetteknologi@gmail.com>
2019-08-27 04:55:58 +0700

    Switch Statement

---
+++ Chapter3-Mastering Javascript/S1.IntroJavascript/15/switch-statement.js
@@ -0,0 +1,14 @@
+    var day = 2
+    switch (day) {
+      case 0:
+        x = "Hari ini minggu"
+        break;
+      case 1:
+        x = "Hari ini senin"
+        break;
+      case 2:
+        x = "Hari ini selasa"
+        break;
+    }
+    //ouput
+    //"Hari ini selasa"
