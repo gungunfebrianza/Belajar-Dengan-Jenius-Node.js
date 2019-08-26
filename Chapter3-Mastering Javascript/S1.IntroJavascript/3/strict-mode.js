6b5abe15ca3c62f3f3ac2d0533b7fa0611f580eb
gungunfebrianza <pusatrisetteknologi@gmail.com>
2019-08-27 05:12:04 +0700

    Update

--- Chapter3-Mastering Javascript/S1.IntroJavascript/3/strict-mode.js
+++ Chapter3-Mastering Javascript/S1.IntroJavascript/3/strict-mode.js
@@ -1,13 +1,13 @@
-// Javascript kode akan dieksekusi dengan mode strict
-'use strict';
+    // Javascript kode akan dieksekusi dengan mode strict
+    'use strict';
 
-// Error akan terjadi karena x belum dideklarasikan
-x = 3.14; //ReferenceError: x is not defined
+    // Error akan terjadi karena x belum dideklarasikan
+    x = 3.14; //ReferenceError: x is not defined
 
-// Use Strict di deklarasikan didalam sebuah function,
-// Strict dalam sekup lokal
-myFunction();
-function myFunction() {
-  ('use strict');
-  y = 3.14; //Error. ReferenceError: y is not defined
-}
+    // Use Strict di deklarasikan didalam sebuah function,
+    // Strict dalam sekup lokal
+    myFunction();
+    function myFunction() {
+      ('use strict');
+      y = 3.14; //Error. ReferenceError: y is not defined
+    }
