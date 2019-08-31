6ec87e5ca7fd839e17bc5ec81308e9f323bd0516
gungunfebrianza <pusatrisetteknologi@gmail.com>
2019-08-31 13:51:53 +0700

    Add Example

---
+++ Chapter5-Mastering-Node.js/S2.JavascriptEngine/allocation-example.js
@@ -0,0 +1,15 @@
+var g = 123; // membuat alokasi memori untuk number
+var f = 'racheysa; // membuat alokasi memori untuk string
+
+var object = {
+  a: 1,
+  b: null
+}; // alokasi memori untuk object dan nilai yang digunakan
+
+// alokasi memori untuk array dan elemen yang digunakanya
+var array = [1, null, 'hazna'];
+
+function f(x) {
+  return x + 10;
+} // alokasi memori untuk fungsi
+
