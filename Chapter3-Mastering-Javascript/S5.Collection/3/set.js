225839df17c9fe8a8e5297e0db38a2cc6c96bad0
gungunfebrianza <pusatrisetteknologi@gmail.com>
2019-10-22 12:02:00 +0700

    Add Seed

---
+++ Chapter3-Mastering-Javascript/S5.Collection/3/set.js
@@ -0,0 +1,1 @@
+const s = new Set()
---
+++ Chapter8-Mastering-MongoDB/seed.js
@@ -0,0 +1,4 @@
+db.artikel.insert({ judul: "Mastering Go", kategori: "ebook", penulis: "Gun Gun Febrianza" })
+db.artikel.insert({ judul: "Mastering Express.js", kategori: "ebook", penulis: "Gun Gun Febrianza" })
+db.artikel.insert({ judul: "Mastering Node.js", kategori: "ebook", penulis: "Gun Gun Febrianza" })
+db.artikel.insert({ judul: "Mastering Javascript", kategori: "ebook", penulis: "Gun Gun Febrianza" })
