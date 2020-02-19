19b4a53b987a197e55c35ff2235d58b2a186c08f
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-02-19 20:28:53 +0700

    update

---
+++ Chapter3-Mastering-Javascript/S1.IntroJavascript/10.multiplication-operator.js
@@ -0,0 +1,6 @@
+/*The multiplication operator produces the product of the operands.*/
+console.log(2 * 2); // 4
+console.log(-2 * 2); // -4
+console.log(Infinity * 0); // NaN
+console.log(Infinity * Infinity); // Infinity
+console.log('foo' * 2); // NaN
