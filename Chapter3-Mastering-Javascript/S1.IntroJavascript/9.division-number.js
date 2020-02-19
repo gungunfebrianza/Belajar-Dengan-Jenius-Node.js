e04d0c43cda6208f680b6a6cb2fe439ad42be6b4
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-02-19 20:27:26 +0700

    Add

---
+++ Chapter3-Mastering-Javascript/S1.IntroJavascript/9.division-number.js
@@ -0,0 +1,7 @@
+/*The division operator produces the quotient of its operands where the left
+    operand is the dividend and the right operand is the divisor.*/
+console.log(1 / 2); // returns 0.5 in JavaScript
+console.log(1.0 / 2.0); // returns 0.5 in both JavaScript and Java
+console.log(2.0 / 0); // returns Infinity in JavaScript
+console.log(2.0 / 0.0); // returns Infinity too
+console.log(2.0 / -0.0); // returns -Infinity in JavaScript
