9114cc47e8856e6bf232c56bb8e2fa3286dc4267
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-02-19 14:32:55 +0700

    AdditionOPeration

---
+++ Chapter3-Mastering-Javascript/S1.IntroJavascript/7.addition-operation.js
@@ -0,0 +1,18 @@
+  //The addition operator produces the sum of numeric operands or string concatenation.
+  // Number + Number -> addition
+  console.log(1 + 2); // 3
+
+  // Boolean + Number -> addition
+  console.log(true + 1); // 2
+
+  // Boolean + Boolean -> addition
+  console.log(false + false); // 0
+
+  // Number + String -> concatenation
+  console.log(5 + 'foo'); // "5foo"
+
+  // String + Boolean -> concatenation
+  console.log('foo' + false); // "foofalse"
+
+  // String + String -> concatenation
+  console.log('foo' + 'bar'); // "foobar"
