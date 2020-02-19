4d46668b09914055f3714b37c73d7125acb566e9
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-02-19 21:16:29 +0700

    update

---
+++ Chapter3-Mastering-Javascript/S1.IntroJavascript/25.AdditionAssignment.js
@@ -0,0 +1,22 @@
+// Assuming the following variables
+//  foo = 'foo'
+//  bar = 5
+//  baz = true
+
+// Number + Number -> addition
+bar += 2; // 7
+
+// Boolean + Number -> addition
+baz += 1; // 2
+
+// Boolean + Boolean -> addition
+baz += false; // 1
+
+// Number + String -> concatenation
+bar += 'foo'; // "5foo"
+
+// String + Boolean -> concatenation
+foo += false; // "foofalse"
+
+// String + String -> concatenation
+foo += 'bar'; // "foobar"
