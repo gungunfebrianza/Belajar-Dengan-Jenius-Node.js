2de46cfd43e57ac8c993986db10d49b7b1f211a2
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-02-19 20:38:52 +0700

    update

---
+++ Chapter3-Mastering-Javascript/S1.IntroJavascript/11.exponentiation.js
@@ -0,0 +1,10 @@
+/*The exponentiation operator returns the result of raising first operand
+    to the power second operand. .*/
+console.log(2 ** 3); // 8
+console.log(3 ** 2); // 9
+console.log(3 ** 2.5); // 15.588457268119896
+console.log(10 ** -1); // 0.1
+console.log(NaN ** 2); // NaN
+console.log(2 ** (3 ** 2)); // 512
+console.log(2 ** (3 ** 2)); // 512
+console.log((2 ** 3) ** 2); // 64
