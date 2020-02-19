1f3aaa3b5b97b888d0d39f03b43f74f72d81516c
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-02-19 14:23:26 +0700

    add

---
+++ Chapter3-Mastering-Javascript/S1.IntroJavascript/6.modulus-operation.js
@@ -0,0 +1,8 @@
+  console.log(12 % 5); // 2
+  console.log(-1 % 2); // -1
+  console.log(1 % -2); // 1
+  console.log(NaN % 2); // NaN
+  console.log(1 % 2); // 1
+  console.log(2 % 3); // 2
+  console.log(-4 % 2); // -0
+  console.log(5.5 % 2); // 1.5
