59fb3b96ec2bd40eb2a40aaf28abc88be404de35
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-02-20 13:43:49 +0700

    update

---
+++ Chapter3-Mastering-Javascript/S1.IntroJavascript/37.clean-code-1.js
@@ -0,0 +1,9 @@
+  var firstName, lastName, price, discount, fullPrice;
+
+  firstName = "John";
+  lastName = "Doe";
+
+  price = 19.90;
+  discount = 0.10;
+
+  fullPrice = price * 100 / discount; 
---
+++ Chapter3-Mastering-Javascript/S1.IntroJavascript/38.clean-code-2.js
@@ -0,0 +1,7 @@
+  var firstName = "",
+    lastName = "",
+    price = 0,
+    discount = 0,
+    fullPrice = 0,
+    myArray = [],
+    myObject = {}; 
