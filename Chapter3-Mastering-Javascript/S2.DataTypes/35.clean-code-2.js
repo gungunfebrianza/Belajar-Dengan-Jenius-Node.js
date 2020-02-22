72d2a74304c668278546e1ed9e7bfe0d12cda696
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-02-22 10:37:52 +0700

    AddCleanCode

---
+++ Chapter3-Mastering-Javascript/S2.DataTypes/35.clean-code-2.js
@@ -0,0 +1,7 @@
+  var v1 = {};           // new object
+  var v2 = "";           // new primitive string
+  var v3 = 0;            // new primitive number
+  var v4 = false;        // new primitive boolean
+  var v5 = [];           // new array object
+  var v6 = /()/;         // new regexp object
+  var v7 = function () { }; // new function object 
