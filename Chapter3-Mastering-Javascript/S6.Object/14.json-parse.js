2d675f4e05cf145d8b3113a094cae9100af95d02
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-02-25 01:34:51 +0700

    AddJSONParse

---
+++ Chapter3-Mastering-Javascript/S6.Object/14.json-parse.js
@@ -0,0 +1,6 @@
+var JSONData = '{"name":"Gun Gun Febrianza","age":28,"city":"Bandung"}'
+
+var objLiteral = JSON.parse(JSONData);
+console.log(typeof objLiteral); //object
+console.log(objLiteral);
+  // { name: 'Gun Gun Febrianza', age: 28, city: 'Bandung' }
