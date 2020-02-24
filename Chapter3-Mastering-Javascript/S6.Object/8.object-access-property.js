d6e09a47b34f6df9bbb1e19167e929a9f235cbea
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-02-25 01:30:00 +0700

    AddObjectAccessProperty

---
+++ Chapter3-Mastering-Javascript/S6.Object/8.object-access-property.js
@@ -0,0 +1,12 @@
+var person = {
+  firstname: 'Gun Gun',
+  lastname: 'Febrianza',
+  age: 28,
+  eyecolor: 'Red Brown',
+  'super loyal': true
+};
+
+console.log(person.firstname + ' is ' + person.age + ' years old.');
+// Gun Gun is 28 years old.
+console.log(person['firstname'] + ' is ' + person['age'] + ' years old.');
+  // Gun Gun is 28 years old.
