50855a1e377f53059675228cd916521548cf39f8
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-02-25 02:54:34 +0700

    AddCleanCodeExample

---
+++ Chapter3-Mastering-Javascript/S2.DataTypes/clean-code-3.js
@@ -0,0 +1,9 @@
+// bad
+var a = 1;
+var b = 2;
+
+// good
+const a = 1;
+const b = 2;
+
+// Why ? This ensures that you can’t reassign your references, which can lead to bugs and difficult to comprehend code.
