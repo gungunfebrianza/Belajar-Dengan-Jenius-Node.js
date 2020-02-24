5a21158f6aea68515432fdffdc55907caf30879a
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-02-25 02:43:26 +0700

    AddLetKeyword

---
+++ Chapter3-Mastering-Javascript/S2.DataTypes/2.let-keyword.js
@@ -0,0 +1,17 @@
+
+var x = 8;
+// Here x is 8
+{
+  let x = 5;
+  // Here x is 5
+  console.log(x);//  
+}
+
+// Here x is 8
+console.log(x); // 8
+
+//another example uncomment
+/* if (true) {
+  let z = 5;
+}
+console.log(z) //error */
