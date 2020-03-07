bc9d32fd0130973634163a13678f705a882abe54
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-03-07 16:26:07 +0700

    AddClassExpression

--- Chapter3-Mastering-Javascript/S8.Classes/4.class-static-method.js
+++ Chapter3-Mastering-Javascript/S8.Classes/4.class-static-method.js
@@ -1,12 +1,12 @@
-  class wallet {
-    constructor(id, balance) {
-      this.id = id;
-      this.balance = balance;
-    }
-    static getBalance() {
-      return 'Your Balance is 0';
-    }
+class wallet {
+  constructor(id, balance) {
+    this.id = id;
+    this.balance = balance;
   }
+  static getBalance() {
+    return "Your Balance is 0";
+  }
+}
 
-  console.log(wallet.getBalance());
-  // Your Balance is 0
+console.log(wallet.getBalance());
+// Your Balance is 0
