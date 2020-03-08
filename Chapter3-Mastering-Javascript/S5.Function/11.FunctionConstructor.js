728e1285d9a009257caae5c6dcb023221e80b2d3
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-03-08 14:11:19 +0700

    UpdateScript

--- Chapter3-Mastering-Javascript/S5.Function/11.FunctionConstructor.js
+++ Chapter3-Mastering-Javascript/S5.Function/11.FunctionConstructor.js
@@ -1,5 +1,18 @@
-  var myFunction = new Function('a', 'b', 'return a * b');
-  console.log(myFunction(4, 3));//12
+function UserCredential(username, password) {
+  this.username = username;
+  this.password = password;
+}
 
-  //rekomendasi
-  var rekomendasiFunction = function expressions(a, b) { return a * b } 
+const user = new UserCredential("Maudy", "indonesia2020");
+console.log(user);
+//UserCredential { username: 'Maudy', password: 'indonesia2020' }
+console.log(user.username); //Maudy
+console.log(user.password); //indonesia2020
+
+var myFunction = new Function("a", "b", "return a * b");
+console.log(myFunction(4, 3)); //12
+
+//rekomendasi
+  var rekomendasiFunction = function expressions(a, b) {
+    return a * b;
+  };
