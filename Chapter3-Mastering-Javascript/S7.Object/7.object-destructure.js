7bd8335d3ab855981329eefcc0d25fd2ddccdb34
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-03-09 01:32:16 +0700

    AddObjectDestructure

---
+++ Chapter3-Mastering-Javascript/S7.Object/7.object-destructure.js
@@ -0,0 +1,11 @@
+const mod = {
+  firstname: "Maudy",
+  lastname: "Ayunda Faza",
+  age: 23,
+  haircolor: "black"
+};
+
+const { firstname, age } = mod;
+
+console.log(firstname, age);
+// Output Maudy 23
