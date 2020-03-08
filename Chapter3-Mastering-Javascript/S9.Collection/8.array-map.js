0b1c42ae28db48c238394e285e8cb1947ffed593
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-03-08 19:05:34 +0700

    AddArrayMap

---
+++ Chapter3-Mastering-Javascript/S9.Collection/8.array-map.js
@@ -0,0 +1,8 @@
+const mod = ["Maudy", "Ayunda Faza", "Gun Gun", "Febrianza"];
+
+const gun = mod.map(item => {
+  return item.toUpperCase();
+});
+
+console.log(gun);
+// [ 'MAUDY', 'AYUNDA FAZA', 'GUN GUN', 'FEBRIANZA' ]
