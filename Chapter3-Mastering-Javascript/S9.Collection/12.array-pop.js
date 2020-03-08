dc4e5b180996db4435bd41a160ff9bcd73688abd
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-03-08 19:11:33 +0700

    AddArrayPop

---
+++ Chapter3-Mastering-Javascript/S9.Collection/12.array-pop.js
@@ -0,0 +1,10 @@
+const mod = ["maudy", "ayunda", "faza"];
+mod.push("cantik");
+console.log(mod.length); // 4
+console.log(mod[3]); // cantik
+console.log(mod);
+// [ 'maudy', 'ayunda', 'faza', 'cantik' ]
+mod.pop();
+console.log(mod);
+// [ 'maudy', 'ayunda', 'faza' ]
+console.log(mod.length); // 3
