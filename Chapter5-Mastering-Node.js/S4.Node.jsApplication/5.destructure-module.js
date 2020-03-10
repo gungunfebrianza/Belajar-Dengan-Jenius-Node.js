5c992fdf17042c0427f5490fcebdf0c3ba677325
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-03-10 14:41:07 +0700

    AddDesctructureStyle

---
+++ Chapter5-Mastering-Node.js/S4.Node.jsApplication/5.destructure-module.js
@@ -0,0 +1,5 @@
+  const { birthday, getName } = require("./3.simple-module.js");
+  console.log(`Maudy birthday is ${birthday}`);
+  // Maudy birthday is 19 December 1994
+  console.log(getName());
+  // Maudy Ayunda 
