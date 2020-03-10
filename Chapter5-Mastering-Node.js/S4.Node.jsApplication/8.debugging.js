c955956033ed0816ecbc6b685ddf0b0af212ad4f
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-03-10 17:08:34 +0700

    AddDebugging

---
+++ Chapter5-Mastering-Node.js/S4.Node.jsApplication/8.debugging.js
@@ -0,0 +1,7 @@
+  const book = {
+    title: "Belajar dengan Jenius AWS & Node.js",
+    price: 80000
+  };
+  const discount = 0.2;
+  const discountPrice = book.price * discount;
+  console.log(`Harga buku : ${book.price - discountPrice}`);
